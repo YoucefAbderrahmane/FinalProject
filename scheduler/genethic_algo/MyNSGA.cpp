8fd8e60cf174cc95bd3568a8922e4913d79ba42d
YoucefAbderrahmane <youcefabderrahmanebekhti@gmail.com>
2016-07-30 16:08:27 +0100

    nouveau fichier: scheduler/genethic_algo/MyNSGA.cpp 	nouveau fichier: scheduler/genethic_algo/MyNSGA.h

---
+++ scheduler/genethic_algo/MyNSGA.cpp
@@ -0,0 +1,21 @@
+/*
+ * MyNSGA.cpp
+ *
+ *  Created on: 12 juil. 2016
+ *      Author: root
+ */
+
+#include "MyNSGA.h"
+
+namespace algorithm {
+
+MyNSGA::MyNSGA() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MyNSGA::~MyNSGA() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace algorithm */
